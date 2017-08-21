e5a6b234c7c12a40bb1166fa341c0edc8c3a3960
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-08-21 01:16:18 -0500

    Relate LTI Users to their Normal Counterparts with a new database table EquivalentUser

--- GitEDU/authApp/models.py
+++ GitEDU/authApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
 # Create your models here.
+
+
+class EquivalentUser(models.Model):
+    normal_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_normal_user")
+    lti_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_lti_user")
