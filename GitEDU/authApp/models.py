8733cb01d574584d68180c6e1f33555c0775c12b
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-08-21 02:12:34 -0500

    Add database tables to associate users with a certain authentication type

--- GitEDU/authApp/models.py
+++ GitEDU/authApp/models.py
@@ -5,5 +5,16 @@ from django.contrib.auth import models as auth_models
 
 
 class EquivalentUser(models.Model):
-    normal_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_normal_user")
-    lti_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_lti_user")
+    normal_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_normal_user",
+                                    null=False)
+    lti_user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, related_name="equivalent_lti_user",
+                                 null=False)
+
+
+class AuthenticationType(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+
+
+class UserAuthentication(models.Model):
+    authentication_type = models.ForeignKey(AuthenticationType, null=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(auth_models.User, on_delete=models.CASCADE, null=False)
