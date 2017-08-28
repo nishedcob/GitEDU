d68d86e9765c0c3d0c84c87d3d99999e2d850470
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-08-27 20:43:00 -0500

    Model basic persistence infrastructure for online code editor

--- GitEDU/ideApp/models.py
+++ GitEDU/ideApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from . import constants
+
+from socialApp.models import Person, Group
+
 # Create your models here.
+
+
+class Repository(models.Model):
+    namespace = models.CharField(max_length=50, null=False)
+    name = models.CharField(max_length=50, null=False)
+    owner = models.ForeignKey(Person, null=False)
+    owning_group = models.ForeignKey(Group, null=True)
+
+
+class File(models.Model):
+    path = models.CharField(max_length=255, null=False)
+    repository = models.ForeignKey(Repository, null=False)
+    language = models.CharField(max_length=50, choices=constants.LANGUAGE_NAMES)
+
+
+class RepositoryPersonMembership(models.Model):
+    person = models.ForeignKey(Person, null=False)
+    repository = models.ForeignKey(Repository, null=False)
+
+
+class RepositoryGroupMembership(models.Model):
+    group = models.ForeignKey(Group, null=False)
+    repository = models.ForeignKey(Repository, null=False)
--- GitEDU/socialApp/models.py
+++ GitEDU/socialApp/models.py
@@ -3,14 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Person(models.Model):
     user = models.ForeignKey(User, null=False)
+
 
 class Student(models.Model):
     person = models.ForeignKey(Person, null=False)
 
+
 class Teacher(models.Model):
     person = models.ForeignKey(Person, null=False)
 
+
 class Administrator(models.Model):
     person = models.ForeignKey(Person, null=False)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+
+class GroupMembership(models.Model):
+    person = models.ForeignKey(Person, null=False)
+    group = models.ForeignKey(Group, null=False)
