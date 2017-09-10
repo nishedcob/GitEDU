dd3bcf2984b6adf5574821478d6d6f191e4027e2
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-09-10 18:59:35 -0500

    Fix Casting Issue when building default alias string

--- GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_connect.py
+++ GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_connect.py
@@ -49,7 +49,7 @@ def build_connection(user=None, password=None, database=None, host=None, port=No
     connection_str = build_connection_string(user=user, password=password, database=database, host=host, port=port,
                                              protocol=protocol)
     if alias is None:
-        alias = MONGODB_ALIAS_STRING + mongodb_conn_num
+        alias = MONGODB_ALIAS_STRING + str(mongodb_conn_num)
         mongodb_conn_num = mongodb_conn_num + 1
     connect(connection_str, alias=alias)
     #connect("mongodb://gitEduUser:G1TedU$3r@127.0.0.1:27017/gitEduDB", alias=alias)
