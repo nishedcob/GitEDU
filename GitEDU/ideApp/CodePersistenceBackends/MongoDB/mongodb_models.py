599a19aab255e54b1f5e26dd343d2469c040d391
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-09-11 16:28:51 -0500

    Build Mongo Models based on Generic Concepts defined for Code Persistence Backends

--- GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_models.py
+++ GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_models.py
@@ -1,18 +1,41 @@
 
 from pymodm import MongoModel
-from pymodm.fields import CharField, IntegerField
+from pymodm.fields import CharField, ReferenceField, TimestampField
 
 from ideApp import constants
-from GitEDU.settings import CODE_PERSISTENCE_BACKENDS, MONGODB_CONNECT_TO
+# from GitEDU.settings import CODE_PERSISTENCE_BACKENDS, MONGODB_CONNECT_TO
 
-class CodeModel(MongoModel):
-    cmid = CharField()
-    orig_cmid = CharField()
-    uid = IntegerField()
-    username = CharField()
-    code = CharField()
+
+class NamespaceModel(MongoModel):
+    name = CharField()
+
+
+class RepositoryModel(MongoModel):
+    name = CharField()
+    namespace = ReferenceField(NamespaceModel)
+
+
+class RepositoryFileModel(MongoModel):
+    contents = CharField()
+    repository = ReferenceField(RepositoryModel)
     language = CharField(choices=constants.LANGUAGE_NAMES)
-    file_name = CharField()
+    file_path = CharField()
 
+    '''
     class Meta:
         connection_alias = CODE_PERSISTENCE_BACKENDS[MONGODB_CONNECT_TO]['connection_profile']
+    '''
+
+
+class ChangeModel(MongoModel):
+    comment = CharField()
+    author = CharField()
+    timestamp = TimestampField()
+    repository = ReferenceField(RepositoryModel)
+
+
+class ChangeFileModel(MongoModel):
+    contents = CharField()
+    change = ReferenceField(ChangeModel)
+    language = CharField(choices=constants.LANGUAGE_NAMES)
+    file_path = CharField()
