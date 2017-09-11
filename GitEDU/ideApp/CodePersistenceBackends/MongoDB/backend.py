6f052edbc10c0eb1bd0d358d5bf3ced314cffc3f
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-09-11 17:01:53 -0500

    Write constructor to connect MongoDB Code Persistence Backend to Database

--- GitEDU/ideApp/CodePersistenceBackends/MongoDB/backend.py
+++ GitEDU/ideApp/CodePersistenceBackends/MongoDB/backend.py
@@ -2,8 +2,15 @@
 from ideApp.CodePersistenceBackends.generics import GenericNamespace, GenericRepository, GenericRepositoryFile,\
     CodePersistenceBackend
 
-#from ideApp.CodePersistenceBackends.MongoDB import mongodb_models, connect
+from ideApp.CodePersistenceBackends.MongoDB import mongodb_models, mongodb_connect
 
+mongo_num_conn = 0
 
 class MongoDBCodePersistenceBackend(CodePersistenceBackend):
-    pass
+
+    def __init__(self, profile=None):
+        global mongo_num_conn
+        if profile is None:
+            raise ValueError("Connection Parameters can not be None")
+        mongodb_connect.build_connection_from_settings(profile, 'mongo_%03d' % mongo_num_conn)
+        mongo_num_conn = mongo_num_conn + 1
