22d3309f14552c1601f93c124fb64ef1e2fb65f3
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-09-13 15:49:53 -0500

    Add String Methods to each Mongo Model

--- GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_models.py
+++ GitEDU/ideApp/CodePersistenceBackends/MongoDB/mongodb_models.py
@@ -9,10 +9,16 @@ from ideApp import constants
 class NamespaceModel(MongoModel):
     name = CharField()
 
+    def __str__(self):
+        return "NamespaceMongoModel: %s" % self.name
+
 
 class RepositoryModel(MongoModel):
     name = CharField()
     namespace = ReferenceField(NamespaceModel)
+
+    def __str__(self):
+        return "RepositoryMongoModel: %s [%s]" % (self.name, self.namespace)
 
 
 class RepositoryFileModel(MongoModel):
@@ -26,12 +32,18 @@ class RepositoryFileModel(MongoModel):
         connection_alias = CODE_PERSISTENCE_BACKENDS[MONGODB_CONNECT_TO]['connection_profile']
     '''
 
+    def __str__(self):
+        return "RepositoryFileMongoModel: %s :: %s :: [%s] :: %s" % (self.file_path, self.language, self.repository, self.contents)
+
 
 class ChangeModel(MongoModel):
     comment = CharField()
     author = CharField()
     timestamp = TimestampField()
     repository = ReferenceField(RepositoryModel)
+
+    def __str__(self):
+        return "ChangeMongoModel: \"%s\" :: %s :: %s :: [%s]" % (self.comment, self.author, self.timestamp, self.repository)
 
 
 class ChangeFileModel(MongoModel):
@@ -40,9 +52,14 @@ class ChangeFileModel(MongoModel):
     language = CharField(choices=constants.LANGUAGE_NAMES)
     file_path = CharField()
 
+    def __str__(self):
+        return "ChangeFileMongoModel: [%s] :: %s :: %s :: %s" % (self.change, self.file_path, self.language, self.contents)
 
 class TemporaryChangeFileModel(MongoModel):
     contents = CharField()
     repository = ReferenceField(RepositoryModel)
     language = CharField(choices=constants.LANGUAGE_NAMES)
     file_path = CharField()
+
+    def __str__(self):
+        return "TemporaryChangeFileMongoModel: [%s] :: %s :: %s :: %s" % (self.repository, self.file_path, self.language, self.contents)
