ae1a4927c06d600b75aa13ff9fde9c1e4010cf58
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-10-22 20:32:26 -0500

    Pymodm Ordered Query Expiriment

--- GitEDU/expir/expir.py
+++ GitEDU/expir/expir.py
@@ -1,6 +1,17 @@
 
+from pymongo import ASCENDING, DESCENDING
 from ideApp.CodePersistenceBackends.MongoDB.mongodb_models import ChangeModel, ChangeFileModel, NamespaceModel,\
     RepositoryModel, RepositoryFileModel
+
+print("Changes in Ascending Order:")
+for change in list(ChangeModel.objects.raw({}).order_by([("timestamp", ASCENDING)])):
+    print(change)
+
+print("Changes in Descending Order:")
+for change in list(ChangeModel.objects.raw({}).order_by([("timestamp", DESCENDING)])):
+    print(change)
+
+'''
 from GitEDU.settings import CODE_PERSISTENCE_BACKEND_MANAGER_CLASS, load_code_persistence_backend_manager
 
 manager = load_code_persistence_backend_manager(CODE_PERSISTENCE_BACKEND_MANAGER_CLASS)
@@ -14,3 +25,4 @@ file_path = "folder/hello.py"
 manager.sync_all()
 
 namespace_objs = manager.get_namespace(namespace=namespace)
+'''
