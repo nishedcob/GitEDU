72c386b4e174ae07646f7a7f7e84567cddd61918
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-10-29 12:06:45 -0500

    Refractor shell executor as Debian Stretch version

--- docker/shell-executor/code/exec.sh
+++ docker/shell-executor/code/exec.sh
@@ -1,4 +1,17 @@
 
+echo ""
+
 whoami
 echo "says hello world!"
 echo "from Docker!"
+
+echo ""
+
+lsb_release -a
+uname -a
+
+echo ""
+
+cat /etc/os-release
+
+echo ""
--- docker/shell-executor/commands-doc.sh
+++ docker/shell-executor/commands-doc.sh
@@ -1,9 +1,13 @@
 
 # build Dockerfile:
-docker build -t nishedcob/shell-executor .
+#######################
+# Debian Stretch
+docker build -t nishedcob/shell-executor:debian-stretch debian/stretch/
 
 # before first run, create a volume:
 docker volume create --name code-shell-executor -o device=`pwd`/code -o o=bind
 
 # to execute once the volume has been created:
-docker run -it --user user --volume "code-shell-executor:/code" nishedcob/shell-executor
+##################################################
+# Debian Stretch
+docker run -it --user user --volume "code-shell-executor:/code" nishedcob/shell-executor:debian-stretch
