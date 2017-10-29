1d911c6ada7a2f10011230c08ddfa80dd8795ed0
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-10-29 12:23:39 -0500

    Add alternative Docker Shell Executor based on Alpine Linux 3.6

--- docker/shell-executor/commands-doc.sh
+++ docker/shell-executor/commands-doc.sh
@@ -3,6 +3,8 @@
 #######################
 # Debian Stretch
 docker build -t nishedcob/shell-executor:debian-stretch debian/stretch/
+# Alpine Linux 3.6
+docker build -t nishedcob/shell-executor:alpine-3.6 alpine/3.6/
 
 # before first run, create a volume:
 docker volume create --name code-shell-executor -o device=`pwd`/code -o o=bind
@@ -11,3 +13,5 @@ docker volume create --name code-shell-executor -o device=`pwd`/code -o o=bind
 ##################################################
 # Debian Stretch
 docker run -it --user user --volume "code-shell-executor:/code" nishedcob/shell-executor:debian-stretch
+# Alpine Linux 3.6
+docker run -it --user user --volume "code-shell-executor:/code" nishedcob/shell-executor:alpine-3.6
