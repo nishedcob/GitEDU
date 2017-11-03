dd1b18c6163bdb15a7e1425e624145dee09c4e8b
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-03 18:29:23 -0500

    PostgreSQL Executor Series and Document construction/upload process to GitLab Registry

--- docker/commands-doc.sh
+++ docker/commands-doc.sh
@@ -8,14 +8,18 @@ docker login registry.gitlab.com
 # Build with correct remote tag:
 docker build -t registry.gitlab.com/nishedcob/gitedu/shell-executor:debian-stretch shell-executor/debian/stretch/
 docker build -t registry.gitlab.com/nishedcob/gitedu/python3-executor:debian-stretch python3-executor/debian/stretch/
+docker build -t registry.gitlab.com/nishedcob/gitedu/postgresql-executor:debian-stretch postgresql-executor/debian/stretch/
 # Push to remote registry with correct remote tag:
 docker push registry.gitlab.com/nishedcob/gitedu/shell-executor:debian-stretch
 docker push registry.gitlab.com/nishedcob/gitedu/python3-executor:debian-stretch
+docker push registry.gitlab.com/nishedcob/gitedu/postgresql-executor:debian-stretch
 # Alpine 3.6
 ##############
 # Build with correct remote tag:
 docker build -t registry.gitlab.com/nishedcob/gitedu/shell-executor:alpine-3.6 shell-executor/alpine/3.6/
 docker build -t registry.gitlab.com/nishedcob/gitedu/python3-executor:alpine-3.6 python3-executor/alpine/3.6/
+docker build -t registry.gitlab.com/nishedcob/gitedu/postgresql-executor:alpine-3.6 postgresql-executor/alpine/3.6/
 # Push to remote registry with correct remote tag:
 docker push registry.gitlab.com/nishedcob/gitedu/shell-executor:alpine-3.6
 docker push registry.gitlab.com/nishedcob/gitedu/python3-executor:alpine-3.6
+docker push registry.gitlab.com/nishedcob/gitedu/postgresql-executor:alpine-3.6
---
+++ docker/postgresql-executor/commands-doc.sh
@@ -0,0 +1,17 @@
+
+# build Dockerfile:
+#######################
+# Debian Stretch
+docker build -t nishedcob/postgresql-executor:debian-stretch debian/stretch/
+# Alpine Linux 3.6
+docker build -t nishedcob/postgresql-executor:alpine-3.6 alpine/3.6/
+
+# before first run, create a volume:
+docker volume create --name code-postgresql-executor -o device=`pwd`/code -o o=bind
+
+# to execute once the volume has been created:
+##################################################
+# Debian Stretch
+docker run -it --volume "code-postgresql-executor:/code" nishedcob/postgresql-executor:debian-stretch
+# Alpine Linux 3.6
+docker run -it --volume "code-postgresql-executor:/code" nishedcob/postgresql-executor:alpine-3.6
