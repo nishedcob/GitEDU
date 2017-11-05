3e0152e68a8de039923eb37615573148ed0c2c38
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-05 12:13:41 -0500

    Database settings for EduNube Development

--- EduNube/EduNube/settings.py
+++ EduNube/EduNube/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'EduNube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'edunubedb',
+        'USER': 'edunubeser',
+        'PASSWORD': '3d?N_6E',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
