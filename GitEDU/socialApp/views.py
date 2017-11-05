88631948265fda84b2de32cead603cb028428bec
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-05 14:49:52 -0500

    Preliminary Verion of SocialApp User View

--- GitEDU/socialApp/views.py
+++ GitEDU/socialApp/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 from django.views import View
@@ -7,5 +9,27 @@ from django.views import View
 
 class UserView(View):  # TODO
 
+    template = 'social/user.html'
+
     def get(self, request, user):
-        return HttpResponse("<html><body><h1>%s</h1></body></html>" % user)
+        username = user
+        same = (request.user.username == username)
+        try:
+            user_obj = User.objects.get(username=user)
+        except ObjectDoesNotExist:
+            raise Http404("User %s does not exist" % username)
+        tiene_codigo = False
+        esCollab = False
+        context = self.build_context(username=username, same=same, user=user_obj, tiene_codigo=tiene_codigo,
+                                     esCollab=esCollab)
+        print("context: %s" % context)
+        return render(request, self.template, context=context)
+
+    def build_context(self, username, same, user, tiene_codigo, esCollab):
+        return {
+            "username": username,
+            "same": same,
+            "user": user,
+            "tiene_codigo": tiene_codigo,
+            "esCollab": esCollab
+        }
