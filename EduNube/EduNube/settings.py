61aef635b18dd36ba665d98607edf58b754466f5
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-05 14:09:07 -0500

    Fix Timezone Issue and add apiApp as an INSTALLED_APP

--- EduNube/EduNube/settings.py
+++ EduNube/EduNube/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apiApp',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +116,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
