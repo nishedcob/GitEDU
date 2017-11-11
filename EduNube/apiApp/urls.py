05147b51a12656f7ca294be1122094836736aa65
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-10 22:13:25 -0500

    Add URL Namespace for apiApp

--- EduNube/apiApp/urls.py
+++ EduNube/apiApp/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 
-import apiApp.urls
-
+appname = "apiApp"
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(apiApp.urls, namespace="api")),
+
 ]
