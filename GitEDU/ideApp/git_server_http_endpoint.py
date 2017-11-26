aa0e35b0ed7899648efe35f006acaf3e569e5f55
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-25 21:47:43 -0500

    Test Edit Namespace Call on GitServerHTTPEndpoint

--- GitEDU/ideApp/git_server_http_endpoint.py
+++ GitEDU/ideApp/git_server_http_endpoint.py
@@ -5,15 +5,18 @@ import requests
 url_template = "%s://%s:%d/api/%s/%s/%s"
 
 object_type = 'ns'
-operation = 'create'
+#operation = 'create'
+operation = 'edit'
 namespace = 'nishedcob2'
+#new_namespace = None
+new_namespace = 'nishedcob3'
 repository = 'test'
 object_path = "%s/" % namespace
 
 url = url_template % (GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('protocol'), GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('host'),
                       GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('port'), object_type, operation, object_path)
 
-payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token')}
+payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token'), 'new_namespace': new_namespace}
 
 print('url: %s' % url)
 print('data: %s' % payload)
