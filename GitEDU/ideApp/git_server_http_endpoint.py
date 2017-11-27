15e6f8f38157b1a68a417e4843f7cd9da198fc19
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-11-26 22:11:42 -0500

    Test Edit Repository Call on GitServerHTTPEndpoint

--- GitEDU/ideApp/git_server_http_endpoint.py
+++ GitEDU/ideApp/git_server_http_endpoint.py
@@ -6,20 +6,22 @@ url_template = "%s://%s:%d/api/%s/%s/%s"
 
 #object_type = 'ns'
 object_type = 'repo'
-operation = 'create'
-#operation = 'edit'
+#operation = 'create'
+operation = 'edit'
 namespace = 'nishedcob2'
 #new_namespace = None
 new_namespace = 'nishedcob3'
 repository = 'test'
+new_repository = 'test2'
 #object_path = "%s/" % namespace
 object_path = "%s/%s/" % (namespace, repository)
 
 url = url_template % (GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('protocol'), GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('host'),
                       GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('port'), object_type, operation, object_path)
 
-payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token')}
+#payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token')}
 #payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token'), 'new_namespace': new_namespace}
+payload = {'token': GIT_SERVER_HTTP_ENDPOINT_CONFIG.get('token'), 'new_namespace': new_namespace, 'new_repository': new_repository}
 
 print('url: %s' % url)
 print('data: %s' % payload)
