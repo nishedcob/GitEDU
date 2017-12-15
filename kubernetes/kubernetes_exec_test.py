e92281c7fbf54d2d7273f9399651edae9e8eab9d
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-12-14 20:14:26 -0500

    Cleanup, improve and document kubernetes code integration test for the sake of clarity

--- kubernetes/kubernetes_exec_test.py
+++ kubernetes/kubernetes_exec_test.py
@@ -1,24 +1,43 @@
 # coding: utf-8
+
+import re
+import time
+
 from apiApp.VirtualizationBackends.Kubernetes import Py3KubernetesVirtualizationBackend
+
+# values for test:
 kvb = Py3KubernetesVirtualizationBackend()
 job_name = 'py3-pytest-3'
 git_repo = 'https://gitlab.com/nishedcob/python3-code-executor-template.git'
+kubernetes_working_tmp_dir = '/tmp'
+
+# prepare test manifest
 manifest = kvb.build_job_template(job_name=job_name, git_repo=git_repo)
-manifest_path = '/tmp/%s.json' % job_name
+manifest_path = '%s/%s.json' % (kubernetes_working_tmp_dir, job_name)
 kvb.write_json_manifest(path=manifest_path, json_data=manifest, overwrite=True)
+
+# create job with manifest
 print(kvb.kubectl_create_from_manifest_file(manifest_path=manifest_path))
+
+# job info
 print(kvb.job_describe(job_id=job_name))
-import re
+
+# get job status & wait until job completion
 detect_zero = re.compile(' 0 ')
 job_get = kvb.job_get(job_id=job_name)
 print(job_get)
 job_get_str = str(job_get[0])
-import time
 while detect_zero.search(job_get_str) is not None:
     time.sleep(1)
     job_get = kvb.job_get(job_id=job_name)
     print(job_get)
     job_get_str = str(job_get[0])
+
+# job info
 print(kvb.job_describe(job_id=job_name))
+
+# job output
 print(kvb.job_logs(job_id=job_name))
+
+# delete job
 print(kvb.kubectl_delete_from_manifest_file(manifest_path=manifest_path))
