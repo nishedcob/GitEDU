08bb41c0c6bff0d5c34cf6c1a01717b486853ac2
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-12-25 21:25:55 -0500

    Create two database tables for JobSpecs (to store and rebuild job manifests) and JobNameCounters (to count the number of times a Job has been executed with minimum effort by the database [we could easily use a COUNT on the previous table, but under heavy load such an operation may not be a good idea] so that a new job can be guaranteed a unique name)

--- EduNube/apiApp/models.py
+++ EduNube/apiApp/models.py
@@ -11,3 +11,15 @@ class RepoSpec(models.Model):
     secret_key = models.CharField(max_length=constants.SECRET_KEY_SIZE, unique=True)
     token = models.CharField(max_length=constants.REPOSPEC_TOKEN_SIZE, unique=True)
     token_algo = models.CharField(max_length=constants.MAX_ALGO_NAME_LENGTH, default=constants.DEFAULT_ALGO)
+
+
+class JobSpec(models.Model):
+    docker_image = models.CharField(max_length=constants.MAX_URL_LENGTH)
+    git_repo = models.CharField(max_length=constants.MAX_URL_LENGTH)
+    job_name = models.CharField(max_length=constants.MAX_NAME_LENGTH, unique=True)
+    deterministic = models.BooleanField()
+
+
+class JobNameCounter(models.Model):
+    orig_job_name = models.CharField(max_length=constants.MAX_NAME_LENGTH, unique=True)
+    job_count = models.IntegerField(default=0)
