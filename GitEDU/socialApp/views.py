72cc0271947a7bd0fce26ec2d393ac1eb4fe80c0
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-12-31 12:36:47 -0500

    List group repositories (determined by owning_group) for a user

--- GitEDU/socialApp/views.py
+++ GitEDU/socialApp/views.py
@@ -28,23 +28,42 @@ class UserView(View):  # TODO
         personal_repos = []
         for personal_repository in personal_repositories:
             personal_repos.append((personal_repository.namespace, personal_repository.name))
+        personal_repos = set(personal_repos)
+        groups = []
+        group_memberships = social_models.GroupMembership.objects.filter(person=person_obj)
+        for group_membership in group_memberships:
+            groups.append(group_membership.group)
+        groups = set(groups)
+        group_repos = []
+        for group in groups:
+            group_repositories = ide_models.Repository.objects.filter(owning_group=group)
+            for_group_repos = {(group_repo.namespace, group_repo.name) for group_repo in group_repositories}
+            for_group_repos = for_group_repos - personal_repos
+            for group_repo in for_group_repos:
+                group_repos.append(group_repo)
+        group_repos = set(group_repos)
+        group_repos = group_repos - personal_repos
         tiene_codigo = False
         esCollab = False
         context = self.build_context(username=username, same=same, user=user_obj, tiene_codigo=tiene_codigo,
-                                     esCollab=esCollab, personal_repos=personal_repos)
+                                     esCollab=esCollab, personal_repos=personal_repos, group_repos=group_repos)
         print("context: %s" % context)
         return render(request, self.template, context=context)
 
-    def build_context(self, username, same, user, tiene_codigo, esCollab, personal_repos):
+    def build_context(self, username, same, user, tiene_codigo, esCollab, personal_repos, group_repos):
         has_personal_repos = len(personal_repos) > 0
+        has_group_repos = len(group_repos) > 0
         context = {
             "username": username,
             "same": same,
             "user": user,
             "tiene_codigo": tiene_codigo,
             "esCollab": esCollab,
-            'has_personal_repos': has_personal_repos
+            'has_personal_repos': has_personal_repos,
+            'has_group_repos': has_group_repos
         }
         if has_personal_repos:
             context['personal_repos'] = personal_repos
+        if has_group_repos:
+            context['group_repos'] = group_repos
         return context
