9f7cfd12a969c1f5d63925c4252dc80979a9d1d0
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2018-01-14 19:26:39 -0500

    Add HOST variable to runserver.sh

--- GitEDU/runserver.sh
+++ GitEDU/runserver.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+HOST = "0.0.0.0"
 PORT_NUMBER="8000"
 PIPENV_COMMAND="pipenv2" # Disable use of pipenv because
 			 # it doesn't install some git
                          # based dependencies correctly?!?
 ENV_COMMAND="/usr/bin/env"
-DJANGO_COMMAND="python manage.py runserver $PORT_NUMBER"
+DJANGO_COMMAND="python manage.py runserver $HOST:$PORT_NUMBER"
 command -v $PIPENV_COMMAND
 if [ $? -eq 0 ]; then
     PIPENV_COMMAND="$ENV_COMMAND $PIPENV_COMMAND"
