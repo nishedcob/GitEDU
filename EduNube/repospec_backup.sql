9498c4aa01e1fa7999de86f2959c384ad8b15739
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2018-01-16 22:31:00 -0500

    Backup RepoSpecs in use before testing RepoSpec API

---
+++ EduNube/repospec_backup.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO edunubeapp."apiApp_repospec" (id,parent_repo,repo,secret_key,token,token_algo) VALUES (
+1,NULL,'python3-code-executor-template','$2b$12$xa959qivezkRs0SKaU2sD.','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXBvIjoicHl0aG9uMy1jb2RlLWV4ZWN1dG9yLXRlbXBsYXRlIiwicGFyZW50X3JlcG8iOm51bGx9.asoWzOYnigG9yhkQf15qi2wualMjwobSWMVItxbihKA','HS256');
+INSERT INTO edunubeapp."apiApp_repospec" (id,parent_repo,repo,secret_key,token,token_algo) VALUES (
+3,NULL,'shell-code-executor-template','$2b$12$nzMqBJeMbKtv6J0i7c9Uz.','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXBvIjoic2hlbGwtY29kZS1leGVjdXRvci10ZW1wbGF0ZSJ9.20iMiHhNgOQvK4-Njnzzn43z7Tidb6GdU0Lp6NO9Fmk','HS256');
+INSERT INTO edunubeapp."apiApp_repospec" (id,parent_repo,repo,secret_key,token,token_algo) VALUES (
+9,NULL,'postgresql-code-executor-template','$2b$12$rfGMx7quNIJ27dEbQG7X2O','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXBvIjoicG9zdGdyZXNxbC1jb2RlLWV4ZWN1dG9yLXRlbXBsYXRlIn0.m02AZDMPvVNn2OnutuCswRUXS4l_KEpU7dzfGEjp8WA','HS256');
