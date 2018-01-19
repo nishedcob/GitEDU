3c26c9fd202fb5ed404e739473dcb5a9fa47395b
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2018-01-19 14:39:32 -0500

    Successful RepoSpec Inheritence Test

--- EduNube/kubernetes_backend_job_create_exec_test.py
+++ EduNube/kubernetes_backend_job_create_exec_test.py
@@ -4,5 +4,7 @@ kvb = Py3KubernetesVirtualizationBackend()
 #kvb.prepare_job(namespace='nishedcob', repository='test', repository_url='http://10.10.10.1/nishedcob/test.git')
 #kvb.execute_job(namespace='nishedcob', repository='test', repository_url='http://10.10.10.1/nishedcob/test.git',
 #                job_name='nishedcob-test')
-kvb.execute_job(namespace='parent', repository='py3-test',
-                repository_url='http://10.10.10.1/python3-code-executor-template.git', job_name='parent-py3-test')
+#kvb.execute_job(namespace='parent', repository='py3-test',
+#                repository_url='http://10.10.10.1/python3-code-executor-template.git', job_name='parent-py3-test')
+kvb.execute_job(namespace='nishedcob', repository='py3-parent-test',
+                repository_url='http://10.10.10.1/nishedcob/test.git', job_name='nishedcob-py3-parent-test')
